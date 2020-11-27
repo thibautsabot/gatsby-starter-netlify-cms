5d7eabf2aff4adc83ec0e4028f925d6f66e95861
Thibaut Sabot <thibaut.sabot@adevinta.com>
2020-11-27 19:23:41 +0100

    update deps and node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,110 +1,114 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://bananeplantee.fr',
-    title: 'Banane Plantee',
-    description:
-      'Le site de cuisine zéro dechet !',
+    siteUrl: "https://bananeplantee.fr",
+    title: "Banane Plantee",
+    description: "Le site de cuisine zéro dechet !",
   },
   plugins: [
-    'gatsby-plugin-robots-txt',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Banane plantee',
-        short_name: 'Banane plantee',
-        start_url: '/',
-        background_color: '#FFFFFF',
-        theme_color: '#008000',
-        display: 'standalone',
-        icon: 'src/img/square-icon.png',
+        name: "Banane plantee",
+        short_name: "Banane plantee",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#008000",
+        display: "standalone",
+        icon: "src/img/square-icon.png",
         icon_options: {
-          purpose: 'any maskable',
+          purpose: "any maskable",
         },
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        indentedSyntax: true,
+      },
+    },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
+        name: "uploads",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
+        name: "images",
       },
     },
     {
-      resolve: 'gatsby-plugin-mailgo',
+      resolve: "gatsby-plugin-mailgo",
       options: {
         mailgoConfig: {
-          lang: 'fr',
+          lang: "fr",
           forceLang: true,
-          showFooter: false
-        }
-      }
+          showFooter: false,
+        },
+      },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
+              name: "uploads",
             },
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 2048,
               withWebp: true,
-              disableBgImage: true
+              disableBgImage: true,
             },
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
+              destinationDir: "static",
             },
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
     {
@@ -117,11 +121,11 @@ module.exports = {
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         defer: true,
-        storage: 'none',
+        storage: "none",
         storeGac: false,
         allowAdFeatures: false,
       },
     },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array,
+    "gatsby-plugin-netlify", // make sure to keep it last in the array,
   ],
-}
+};

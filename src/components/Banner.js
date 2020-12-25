9e7045b8b9c627a7f18cc05852474ed7c9812e00
thibaut.sabot <thibaut.sabot@gmail.com>
2020-12-25 16:53:16 +0100

    install gatsby next versions

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image"
+import banner from '../img/banner.png'
 
 class Banner extends React.Component {
   render() {
     return (
+      <>
+      <p>coucou</p>
+      <StaticImage width={100} height={100} src="../img/banner.png" alt='banniere banane plantee' />
       <div
         role="banner"
-        className="full-width-image-container margin-top-0"
-        style={{
-          backgroundImage: `url(${require("../img/banner.png")})`,
-          backgroundPositionY: "15%",
-        }}
+        // className="full-width-image-container margin-top-0"
+        // style={{
+        //   backgroundImage: `url(${require("../img/banner.png")})`,
+        //   backgroundPositionY: "15%",
+        // }}
       >
         <h1
           className="has-text-weight-bold banner-text"
@@ -23,6 +28,7 @@ class Banner extends React.Component {
           {this.props.title}
         </h1>
       </div>
+      </>
     );
   }
 }

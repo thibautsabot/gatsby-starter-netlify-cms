b46639dfe5d010217cdfc2987143e72c9a1e6c25
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-02-07 16:42:17 +0100

    rollback size

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,10 +89,10 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 1500,
+              maxWidth: 2048,
               tracedSVG: true,
               withWebp: true,
-              withAvif: true,
+              // withAvif: true,
               // disableBgImage: true, /* enable bg images for traced svg */
             },
           },
